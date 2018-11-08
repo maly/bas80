4cf1fe6a22deb9a2aacfc0dda00bb89d794aec1a
Martin Maly <maly@maly.cz>
2018-11-08 15:44:50 +0100

    Kilo

---
+++ targets/omenkilo/cfg.js
@@ -0,0 +1,36 @@
+var CONFIG = CONFIG || {};
+CONFIG.OMENKILO= {
+        org:"0100h",
+        ramtop:"07f00h",
+        goback:"JMP [$FFFE]",
+        cpu:"M6809",
+        init:"\tLXI H,0\n\tDAD SP\n\tSHLD savesp\n",
+        system: {
+            "serout": {
+                uses:null,
+                sysdb:["prtchan"],
+                code: "\tPUSH PSW\n"+
+                "so_wait: IN 0deh ;acias\n"+
+                "\tani 2\n"+
+                "\tjz so_wait\n"+
+                "\tpop psw ;aciad\n"+
+                "\tout 0dfh\n"+
+                "\tRET\n"
+            },
+            "serin": {
+                uses:["serout"],
+                sysdb:["prtchan","inpchan"],
+                code: "\tIN 0deh ;acias\n"+
+                "\tani 1\n"+
+                "\trz\n"+
+                "\tin 0dfh ;aciad\n"+
+                "\tcall serout\n"+
+                "\tora a\n"+
+                "\tRET\n"
+            }
+        },
+        xp:{},
+        asm:{}
+    }
+
+if (typeof module != 'undefined') module.exports=CONFIG
